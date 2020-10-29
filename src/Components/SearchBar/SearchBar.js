abee673ce8bcc33e918e54589ee5c22f8b4e99e4
Elad Karni <elad.karni@gmail.com>
2020-10-28 20:21:02 -0400

    removed clg

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -37,7 +37,7 @@ const SearchBar = props => {
 		}
 		getPokemonAPI()
     }, [])
-
+    console.log(data)
   return (
     <div className={styles.searchBar} id={styles.search}>
       <Typeahead className={styles.typeahead} {...typeaheadProps} />
