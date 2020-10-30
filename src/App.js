52918310aaa50919ada03a893c45241508e4610c
Elad Karni <elad.karni@gmail.com>
2020-10-30 18:03:59 -0400

    ESLint Clean-up App.js & app.test.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from "react-router-dom";
-import ListPage from "./Pages/ListPage/ListPage";
-import MenuPage from "./Pages/MenuPage/MenuPage";
-const App = () => {
+} from 'react-router-dom';
+import ListPage from './Pages/ListPage/ListPage';
+import MenuPage from './Pages/MenuPage/MenuPage';
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <MenuPage />
-        </Route>
-        <Route path="/:listID">
-          <ListPage />
-        </Route>
-      </Switch>
-    </Router>
-  )
-  
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <MenuPage />
+      </Route>
+      <Route path="/:listID">
+        <ListPage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
