99ea9d4f7b2679b34d9c675a6a7c1ff2b5485041
Elad Karni <elad.karni@gmail.com>
2020-10-30 18:06:08 -0400

    ESLint Clean-up MenuPage and changed Form.Control

--- src/Pages/MenuPage/MenuPage.js
+++ src/Pages/MenuPage/MenuPage.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
-import { Button, Form, InputGroup } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
 import styles from './MenuPage.module.scss';
 
 function MenuPage() {
-    const [sCode, setSCode] = useState('')
-    const [err, setErr] = useState('')
+  const [sCode, setSCode] = useState('');
+  const [err, setErr] = useState('');
 
-    const history = useHistory();
-    const handleClick = () => sCode.length > 0 ? history.push(`/SL-${sCode}`) : setErr("Please Add A Code")
+  const history = useHistory();
+  const handleClick = () => (sCode.length > 0 ? history.push(`/SL-${sCode}`) : setErr('Please Add A Code'));
 
-    return (
-        <div className={styles.menuPage}>
-            <div className={styles.actionArea}>
-                <Form className={styles.form}>
-                    <Form.Label>Soul-Link Code</Form.Label>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon3">
-                            {`SL-`}
-                        </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control id="basic-url" aria-describedby="basic-addon3" />
-                    </InputGroup>
-                    <Button onClick={handleClick}>Join</Button>
-                </Form>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.menuPage}>
+      <div className={styles.actionArea}>
+        <Form className={styles.form}>
+          <Form.Label>Soul-Link Code</Form.Label>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon3">
+                SL-
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control onChange={(e) => setSCode(e.target.value)} type="text" placeholder="423f2gf7" />
+            <span>{err}</span>
+          </InputGroup>
+          <Button onClick={handleClick}>Join</Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default MenuPage
+export default MenuPage;
