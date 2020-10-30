eed7f9d7d472e340fa5eb9505fd9f3320929756d
Elad Karni <elad.karni@gmail.com>
2020-10-30 18:04:54 -0400

    ESLint Clean-up Header and added prop-types check

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,35 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import PropTypes from 'prop-types';
 
-import "./Header.scss";
+import './Header.scss';
 
-const Header = props => {
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">
-        {" Soul-Link PokeApp"}
-      </Navbar.Brand>
-      {props.code !== "" ? props.code : null}
-      <Form inline onSubmit={(e) => e.preventDefault()}>
-        <FormControl
-          className="search-input"
-          type="text"
-          placeholder="Search for Nickname, Pokemon, or Type"
-          value={props.filterText}
-          disabled={props.disabledInput}
-          onChange={props.change}
-        />
-      </Form>
-    </Navbar>
-  );
+const Header = ({
+  filterText, disabledInput, change,
+}) => (
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/">
+      {' Soul-Link PokeApp'}
+    </Navbar.Brand>
+    <Form inline onSubmit={(e) => e.preventDefault()}>
+      <FormControl
+        className="search-input"
+        type="text"
+        placeholder="Search for Nickname, Pokemon, or Type"
+        value={filterText}
+        disabled={disabledInput}
+        onChange={change}
+      />
+    </Form>
+  </Navbar>
+);
+
+Header.propTypes = {
+  filterText: PropTypes.string.isRequired,
+  disabledInput: PropTypes.bool.isRequired,
+  change: PropTypes.func.isRequired,
 };
 
 export default Header;
