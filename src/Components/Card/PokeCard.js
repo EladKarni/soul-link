1ce7160a3f2cf7eb166f236d080b25b2a0e75607
Elad Karni <elad.karni@gmail.com>
2020-11-02 01:28:46 -0500

    refactored PokeCard styles & inline text edit

--- src/Components/Card/PokeCard.js
+++ src/Components/Card/PokeCard.js
@@ -1,11 +1,62 @@
-import React from 'react';
-// import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import Tilt from 'react-parallax-tilt';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import firebase from 'firebase';
 import styles from './PokeCard.module.scss';
+import Editable from '../Editable/Editable';
 
 const PokeCard = (props) => {
-  const { pair, unlinked } = props;
+  const { card, unlinked } = props;
+  const [title, setTitle] = useState('');
+  const [nickname, setNickname] = useState([]);
+  const { listID } = useParams();
+
+  const titleSubmit = (e, value) => {
+    firebase.firestore().collection(listID).doc(card.id).update({ title: value });
+  };
+
+  const nickSubmit = (e, value, index) => {
+    firebase
+      .firestore()
+      .runTransaction(async (transaction) => {
+        let document = await transaction.get(
+          firebase
+            .firestore()
+            .doc(`${listID}/${card.id}`),
+        );
+        document = document.data();
+        document.pokemon[index].nickname = value;
+        transaction.update(
+          firebase
+            .firestore()
+            .doc(`${listID}/${card.id}`),
+          {
+            pokemon: document.pokemon,
+            last_updated: new Date().getTime(),
+          },
+        );
+      });
+  };
+
+  const changeNickname = (value, index) => {
+    setNickname(
+      nickname.map((n, i) => {
+        if (i !== index) return n;
+        return value;
+      }),
+    );
+  };
+
+  const changeTitle = (value) => {
+    setTitle(value);
+  };
+
+  useEffect(() => {
+    setNickname(card.pokemon.map((poke) => poke.nickname));
+    setTitle(card.title);
+  }, [card]);
+
   return (
     <Tilt
       className={styles.parallaxEffect}
@@ -18,11 +69,24 @@ const PokeCard = (props) => {
 
       <div className={styles.card}>
         <div className={styles.title}>
-          <h2>Title</h2>
+          {title.length > 0
+            ? (
+              <Editable
+                text={title}
+                placeholder={card.title}
+                type="input"
+                cssStyle={styles.inlineInput}
+                listCode={listID}
+                changeText={changeTitle}
+                cardID={card.id}
+                syncFunc={titleSubmit}
+              />
+            )
+            : null }
         </div>
         <div className={styles.sprite}>
           <div className={styles.circle} />
-          {pair.pokemon.map(({ sprites }) => (
+          {card.pokemon.map(({ sprites }) => (
             <img
               key={sprites.front_default}
               src={sprites.front_default}
@@ -31,11 +95,25 @@ const PokeCard = (props) => {
           ))}
         </div>
         <div className={styles.infoGroup}>
-          {pair.pokemon.map(({ name: pokemon, types }) => (
+          {card.pokemon.map(({ name: pokemon, types }, index) => (
             <div className={styles.info} key={pokemon}>
-              <h1 className={styles.nickname}>
-                nickname
-              </h1>
+              <div className={styles.nickname}>
+                {nickname[index]?.length > 0
+                  ? (
+                    <Editable
+                      text={nickname[index]}
+                      placeholder={card.nickname}
+                      type="input"
+                      cssStyle={styles.nickname}
+                      listCode={listID}
+                      changeText={changeNickname}
+                      cardID={card.id}
+                      syncFunc={nickSubmit}
+                      index={index}
+                    />
+                  )
+                  : null}
+              </div>
               <div className={styles.name}>{pokemon}</div>
               <div className={styles.types}>
                 {types.map(({ type: { name: Poketype } }) => (
@@ -51,7 +129,7 @@ const PokeCard = (props) => {
           <button
             type="button"
             onClick={() => {
-              unlinked(pair.id);
+              unlinked(card.id);
             }}
           >
             Destroy Link
@@ -63,7 +141,7 @@ const PokeCard = (props) => {
 };
 
 PokeCard.propTypes = {
-  pair: PropTypes.object.isRequired,
+  card: PropTypes.object.isRequired,
   unlinked: PropTypes.func.isRequired,
 };
 
