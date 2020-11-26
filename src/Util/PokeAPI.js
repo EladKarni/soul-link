b75dcd9e893dab36e1a90850385a23fef5534821
Elad Karni <elad.karni@gmail.com>
2020-11-26 00:21:54 -0500

    readded nickname when creating pokemon objects

--- src/Util/PokeAPI.js
+++ src/Util/PokeAPI.js
@@ -5,7 +5,9 @@ const { getPokemonByName } = new Pokedex();
 const GetPokemonFromPokeAPI = async (selected) => {
   const getDetailsSortedBySlot = (pokemon) => {
     const { name, types, sprites } = pokemon;
-    return { name, sprites, types: types.sort((a, b) => a.slot - b.slot) };
+    return {
+      nickname: 'Edit Me', name, sprites, types: types.sort((a, b) => a.slot - b.slot),
+    };
   };
 
   const pokemons = await Promise.all(selected.map((pokemon) => getPokemonByName(pokemon.name)));
